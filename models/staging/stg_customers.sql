415c19ff86fca08b4ffc316bbfd50502e3544597
Deniz Günay <denizeren.deg505@gmail.com>
2025-05-31 21:00:26 +0000

    stg_customers fixed

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,12 +1,9 @@
 {{ config(materialized='table') }}
 
-with stg_customers AS (
-   SELECT
-      customer_id,
-      CONCAT(first_name, ' ', last_name) as customer_name,
-      email as email_address,
-      address as billing_address
-   FROM raw.customers
-)
+SELECT
+    customer_id,
+    CONCAT(first_name, ' ', last_name) as customer_name,
+    email as email_address,
+    address as billing_address
+FROM raw.customers
 
-select * from stg_customers
